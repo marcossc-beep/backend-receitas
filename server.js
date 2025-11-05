bea6e1d989b7ee00aa480471f04e647a70a8f9f4
unknown <Professor@SN-396949.edu.fiesc.com.br>
2025-11-05 16:46:29 -0300

    count em categorias

--- server.js
+++ server.js
@@ -89,7 +89,10 @@ server.post('/usuarios', async (req, reply) => {
 server.get('/categorias', async (req, reply) => {
     try {
         const resultado = await pool.query('SELECT * FROM categorias')
-        reply.status(200).send(resultado.rows)
+        
+        const count = await pool.query("SELECT COUNT(*) FROM categorias")
+        reply.status(200).send({data: resultado.rows, count: parseInt(count.rows[0].count) })
+
     } catch (err) {
         reply.status(500).send({ error: err.message })
     }
